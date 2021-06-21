fd8ed173b074ce8723c872c6d38b3b6a2a527afa
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-06-21 12:51:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -244,6 +244,13 @@ resource "aws_security_group" "sg_bastion_host" {
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
    }
+   ingress {
+      description = "allow cloudMapper"
+      from_port = 8000
+      to_port = 8000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
    egress {
       from_port = 0
       to_port = 0
